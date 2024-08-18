39fd6d38b8dada890587015132ed39f3fb103d14
AijazAhmedBZ <aijaz.ahmed.bz@gmail.com>
2024-08-18 08:31:25 +0500

    update on 18-Aug-24

---
+++ 01_basics/hello_tea.py
@@ -0,0 +1,10 @@
+print("Study python on 17-Aug-24")
+
+def teas(n):
+    print(n)
+
+print("Lemon Tea")
+
+tea1 = ("Lemon Tea")
+tea2 = ("Ginger Tea")
+tea3 = ("Masala Tea")
---
+++ 01_basics/tea.py
@@ -0,0 +1,3 @@
+from hello_tea import teas
+
+teas("Now Printing Coding from hello_tea.py")
